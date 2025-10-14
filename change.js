f6e40d0ac670a54e455876be25f70331cb992d7f
justreana <khami.rea.dullina@mail.ru>
2025-10-14 18:55:57 +0300

    Update change.js

--- change.js
+++ change.js
@@ -1,13 +1,30 @@
 const change = document.getElementById("change")
 
 function change_all(){
-document.querySelector(".block4").innerText = "Khamidullina";
-  document.querySelector(".block5").innerText = "Reana";
-  document.querySelector(".block6").innerText = "Airatovna";
-  document.querySelector(".block7").innerText = "female";
-  document.querySelector(".block9").innerText = "Kazan";   
-    
+    if (document.getElementsByTagName('div')[3].innerText==='Khamidullina'){
+        document.getElementsByTagName('div')[3].innerText='Хамидуллина';
+        document.getElementsByTagName('div')[4].innerText='Реана';
+        document.getElementsByTagName('div')[5].innerText='Айратовна';
+        document.getElementsByTagName('div')[6].innerText='жен';
+        document.getElementsByTagName('div')[8].innerText='Казань';
+    }
+    else{
+
+        if (document.getElementsByTagName('div')[3].innerText==='Хамидуллина'){
+            document.body.style.backgroundImage = "url('eng_pas.jpg')";
+            document.getElementsByTagName('div')[3].innerText='Khamidullina';
+            document.getElementsByTagName('div')[4].innerText='Reana';
+            document.getElementsByTagName('div')[5].innerText='Airatovna';
+            document.getElementsByTagName('div')[6].innerText='female';
+            document.getElementsByTagName('div')[8].innerText='Kazan';
+            document.getElementsByTagName('div')[5].style.marginLeft='200px';
+            document.getElementsByTagName('div')[3].style.marginTop='200px';
+
+
+
+        }
+    }
 }
 
-
 change.addEventListener("click", change_all)
+
